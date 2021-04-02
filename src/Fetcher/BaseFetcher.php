4e37c9e864920dfdb5cd4ef0fbd1e2aee0622bee
Bohdan Khandruk <bkhandruk@gmail.com>
2021-04-02 12:31:28 +0300

    Made fetcher more precise

--- src/Fetcher/BaseFetcher.php
+++ src/Fetcher/BaseFetcher.php
@@ -8,7 +8,15 @@ class BaseFetcher extends AbstractFetcher
     public function suggestId($string)
     {
         $data = $this->fetch('/title/find', ['q' => $string]);
-        $id = isset($data->results[0]->id) ? $data->results[0]->id : '';
+
+        $index = 0;
+        foreach ($data->results as $key => $result) {
+            if ($result->title == $string) {
+                $index = $key;
+            }
+        }
+
+        $id = isset($data->results[$index]->id) ? $data->results[$index]->id : '';
 
         if (preg_match('/^\/(?>name|title)\/(.+)\/$/', $id, $matches)) {
             return $matches[1];
